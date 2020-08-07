f843cb8903f580bdc9bdcfb4eab915f112e9baf0
shtjrgus010 <shtjrgus010@naver.com>
2020-08-07 16:37:30 +0900

    Saving the User Name part Two

--- gretting.js
+++ gretting.js
@@ -5,15 +5,34 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  //사용자를 기억
+  localStorage.setItem(USER_LS, text);
+}
+function handleSubmit() {
+  //form형식을 제출할때 event
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+function askForName() {
+  //사용자입력 받기
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
 function paintGreeting(text) {
+  //화면에 표시
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
+  //사용자가 있는지 없는지
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser == null) {
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
