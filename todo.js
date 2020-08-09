7b5e1f9cec9a946c3fe04e4b012410eb55359e6c
shtjrgus010 <shtjrgus010@naver.com>
2020-08-09 21:33:19 +0900

    to do list part 2 and Image Background

--- todo.js
+++ todo.js
@@ -3,7 +3,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos"; //local storage
-const toDos = [];
+
+function filterFn(toDo) {}
+let toDos = [];
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return console.log(toDo.id !== parseInt(li.id));
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
@@ -14,6 +26,7 @@ function paintToDo(text) {
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
